c74fd72ed4378bd8d02b9af717d7c321d000443b
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-30 19:01:47 +0530

    Update Data.jsx

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,13 +1,24 @@
 export default function Data({ data }) {
+  const iconCode = data?.weather[0]?.icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
   return (
     <div>
       <p className="temp">{data?.main?.temp}°C</p>
-      <p className="cloud-info">{data?.weather[0]?.description}</p>
-      <div className="other-info">
+      <div className="cloud-info">
+        <img
+          src={iconUrl}
+          alt={data?.weather[0]?.description}
+          className="cloud-icon"
+        />
+        <span className="cloud-data">{data?.weather[0]?.description}</span>
+      </div>
+      <div className="weather-info">
         <div className="humidity">
           <span>{data?.main?.humidity}% </span>
           <span>Humidity</span>
         </div>
+        <div className="mid-line"></div>
         <div className="wind-speed">
           <span>{data?.wind?.speed} m/s </span>
           <span>Wind Speed</span>
