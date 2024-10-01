b49c83a2c41c28730f2c200d61845db3597b063e
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-02 01:42:12 +0530

    Update Display.jsx

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,8 @@
 import Data from "./Data.jsx";
 
 export default function Display({ data }) {
+
+  // Function to get the current date
   function getCurrentDate() {
     return new Date().toLocaleDateString("en-us", {
       weekday: "long",
@@ -12,10 +14,13 @@ export default function Display({ data }) {
 
   return (
     <div>
+      {/* Display the location (city and country) */}
       <h2 className="place">
         {data?.name}, {data?.sys?.country}
       </h2>
+      {/* Display the current date */}
       <p className="date">{getCurrentDate()}</p>
+      {/* Render the Data component */}
       <Data data={data} />
     </div>
   );
